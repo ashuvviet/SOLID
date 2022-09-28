ab94a353e3623c95c7c8f08acc3c84cf7c9b29a1
Ashutosh Gupta <ashutosh.gupta@sciex.com>
2022-09-29 00:09:09 +0530

    Associates

---
+++ Manjula/EmployeeManagementApi/Controllers/EmployeeController.cs
@@ -0,0 +1,102 @@
+﻿using EmployeeManagementApi.Dto;
+using EmployeeManagementApi.Managers;
+using EmployeeManagementApi.Models;
+using EmployeeManagementApi.Options;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeDBContext employeeDBContext;
+        private readonly IOptions<DbConfig> dbOptions;
+
+        public EmployeeController(EmployeeDBContext employeeDBContext, IOptions<DbConfig> dbOptions)
+        {
+            this.employeeDBContext = employeeDBContext;
+            this.dbOptions = dbOptions;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = new List<Employee>();
+            using (var connection = new SqlConnection(this.dbOptions.Value.PathToDB))
+            {
+                connection.Open();
+                var employeeQuery = "Select * from Employees";
+                var cmd = new SqlCommand(employeeQuery, connection);
+
+                var read = cmd.ExecuteReader();
+                while (read.Read())
+                {
+                    var e = new FullTimeEmployee();
+                    e.Id = (int)read["id"];
+                    e.FirstName = (string)read["FirstName"];
+                    e.LastName = (string)read["LastName"];
+                    e.Email = (string)read["Email"];
+                    result.Add(e);
+                }               
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> InsertEmployee(EmployeeDto employeeDto)
+        {
+            if(string.IsNullOrEmpty(employeeDto.FirstName) || string.IsNullOrEmpty(employeeDto.LastName))
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (string.IsNullOrEmpty(employeeDto.Email) || !employeeDto.Email.Contains("@"))
+            {
+                throw new InvalidOperationException();
+            }
+
+            var result = false;
+            using (var connection = new SqlConnection(this.dbOptions.Value.PathToDB))
+            {
+                connection.Open();
+                var employeeQuery = "Insert into Employees (FirstName,LastName,Email,BasicPay,HRA,Bonus,IsFullTimeEmployee, EmpType) VALUES (@1,@2,@3,@4,@5,@6, @7, @8)";
+
+                result = ExecuteQueryWithNoResult(connection, employeeQuery, employeeDto.FirstName, employeeDto.LastName, employeeDto.Email,1,1,1,false,1);
+            }
+
+            // send mail to finance / insurance team
+            var mailService = new SMTPMailService();
+            await mailService.SendMail("finance@danaher.com", "Welcome", "Welcome To Danaher");
+
+            return Ok(result);
+        }
+
+        private bool ExecuteQueryWithNoResult(SqlConnection connection, string query, params object[] paramList)
+        {
+            var cmd = new SqlCommand(query, connection);
+
+            var paramCount = 0;
+            foreach (var param in paramList)
+            {
+                var myParam = new SqlParameter(string.Format("@{0}", ++paramCount), SqlDbType.VarChar)
+                {
+                    Value = param
+                };
+                cmd.Parameters.Add(myParam);
+            }
+
+            // return a Sql data reader that returns number of impacted lines
+            return cmd.ExecuteNonQuery() > 0;
+        }
+    }
+
+   
+}
---
+++ RaviSingh/EmployeeManagementApi/Managers/SMTPMailService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementApi.Managers
+{
+    public class SMTPMailService
+    {
+        public async Task<bool> SendMail(string to, string subject, string body)
+        {
+            try
+            {
+                SmtpClient client = new SmtpClient();
+                client.Host = "<host>";
+                client.Port = int.MaxValue;
+
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential("<username>", "<password>");
+
+                MailMessage message = CreateMailMessage(to, subject, body);
+                client.Send(message);
+                return await Task.FromResult(true);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private MailMessage CreateMailMessage(string to, string subject, string body)
+        {
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress("<registered mail id>");
+            mailMessage.To.Add(to);
+            mailMessage.Body = body;
+            mailMessage.Subject = subject;
+            return mailMessage;
+        }
+    }
+}
