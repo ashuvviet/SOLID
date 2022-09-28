1e7264bf4298cab67be89bd5471022e5d80d2d66
Ashutosh Gupta <ashutosh.gupta@sciex.com>
2022-09-29 00:00:51 +0530

    final one

---
+++ Final/Employee.Api/Startup.cs
@@ -0,0 +1,61 @@
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+using Core.Contracts;
+using Core.Options;
+using DataBaseCore.Extensions;
+using DataBaseCore;
+using MailService.Extensions;
+
+namespace EmployeeWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.Configure<DbConfig>(Configuration);
+
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+                    
+            services.RegisterDatabase();
+            services.RegisterMailService();
+            services.AddSingleton<INamingService, NamingService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //applicationDbContext.Database.Migrate();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ Final/LiteDbServer/Extension/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using DataBaseCore.DBContext;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataBaseCore.Extensions
+{
+    public static class Extensions
+    {
+        public static void RegisterDatabase(this IServiceCollection services, string pathToDB)
+        {
+            services.AddSingleton<LiteDBContext>();
+        }
+    }
+}
