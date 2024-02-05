7e47c2f8d031f7d1561b4464c7f7bd213a1cc029
Ashutosh Gupta <ashutosh.gupta@sciex.com>
2024-02-05 20:26:01 +0530

    remove unsed namespace

--- Ashutosh/SRP_DI/EmployeeManagementApi/Controllers/EmployeeController.cs
+++ Ashutosh/SRP_DI/EmployeeManagementApi/Controllers/EmployeeController.cs
@@ -9,7 +9,6 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Threading.Tasks;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace EmployeeManagementApi.Controllers
 {
